ff2e0f7d30bc0b350d0f41b4ca0466b849d3917c
Hasnine Mamud <hasninehemel85@gmail.com>
2024-11-18 11:40:40 +0600

    add delete function on DLL

--- Linked List/Double_LinkedList.py
+++ Linked List/Double_LinkedList.py
@@ -61,16 +61,73 @@ class DLL:
             temp = temp.next
         print("None")
 
+    def delete_first(self):
+        if self.start is not None:
+            self.start = self.start.next  
+            if self.start is not None:
+                self.start.prev = None
+    
+    def delete_last(self):
+       
+        if self.start is None:
+            pass
+        elif self.start.next is None:
+            self.start = None
+        else:
+            temp = self.start
+            while temp.next.next != None:
+                temp = temp.next
+            temp.next = None
+    
+    def delete_item(self, data):
+        if self.start is None:
+            print("List is empty")
+            return
+
+    # If the first node contains the data
+        if self.start.item == data:
+            if self.start.next is None:
+            # If there's only one node
+                self.start = None
+            else:
+            # Move the start pointer to the next node
+                self.start = self.start.next
+                self.start.prev = None
+            return
+
+    # Traverse the list to find the node
+        temp = self.start
+        while temp is not None and temp.item != data:
+            temp = temp.next
+
+    # If the data is not found
+        if temp is None:
+            print("Data Not Found")
+            return
+
+    # If the data is found
+        if temp.next is not None:
+            temp.next.prev = temp.prev
+        if temp.prev is not None:
+            temp.prev.next = temp.next
+
+    
             
-
-
+            
+        
 myList = DLL()
 
 myList.insert_at_start(10)
 myList.insert_at_start(20)
 myList.insert_at_start(30)
 myList.insert_at_last(40)
-myList.insert_after(20, 25)
-
+# myList.insert_after(20, 25)
 myList.display()
-print(myList.is_empty())
+# myList.delete_first()
+# myList.display()
+# myList.delete_last()
+# myList.display()
+
+myList.delete_item(50)
+myList.display()
+# print(myList.is_empty())
